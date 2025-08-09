280a63956a8f1af0eac84f609ef8b88536802aa3
Archasmiel <archasmiel@gmail.com>
2025-08-09 13:13:06 +0300

    rrrr

--- src/wifi_setup.cpp
+++ src/wifi_setup.cpp
@@ -1,13 +1,18 @@
 #include <WiFi.h>
 #include <WiFiManager.h>
+#include <cstring>            // for strlen
 #include "lcd_display.h"
 #include "secrets.h"
 
 void setupWiFiWithPortal(const String &deviceName) {
   WiFi.mode(WIFI_STA);
   WiFiManager wm;
-  if (strlen(WM_AP_NAME) > 0) {
+
+  if (deviceName.length()) {
     wm.setHostname(deviceName.c_str());
+  }
+
+  if (strlen(WM_AP_NAME) > 0) {
     showWiFiPortal();
     bool ok = wm.autoConnect(WM_AP_NAME, strlen(WM_AP_PASS) ? WM_AP_PASS : nullptr);
     if (!ok) {
@@ -15,5 +20,7 @@ void setupWiFiWithPortal(const String &deviceName) {
       delay(2000);
       ESP.restart();
     }
+    // nice to show IP on success
+    lcdPrint2("WiFi OK", WiFi.localIP().toString());
   }
 }
